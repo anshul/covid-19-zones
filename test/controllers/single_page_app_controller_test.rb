e627800a017cf13c75fd3009783856106a201bb9
Danish Shah <shah.danish.1996@gmail.com>
2020-04-14 00:02:48 +0530

    feat(rails): add rake task for fetching raw data and add seeds for zones

--- test/controllers/single_page_app_controller_test.rb
+++ test/controllers/single_page_app_controller_test.rb
@@ -1,9 +1,10 @@
-require 'test_helper'
+# frozen_string_literal: true
+
+require "test_helper"
 
 class SinglePageAppControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get single_page_app_index_url
     assert_response :success
   end
-
 end
