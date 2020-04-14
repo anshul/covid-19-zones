ef2bbf1064873f27c8666a2c935772957c5cdb2e
Jawad Shaikh <shaikhjawad007@gmail.com>
2020-04-14 17:26:16 +0530

    improvement(rails): make patient fetcher idempotent

--- app/commands/fetch_raw_data.rb
+++ app/commands/fetch_raw_data.rb
@@ -34,51 +34,92 @@ class FetchRawData
     json_data["raw_data"].each do |row|
       puts_yellow "Parsing row for patient number ##{row['patientnumber']}"
 
-      patient_number = row["patientnumber"]
-      slug = "covid19-india-#{patient_number}"
-      code = "c19-in-#{patient_number}"
-      zone_code = nil
+      patient_already_exists = Patient.exists?(code: Patient.code_for(row["patientnumber"]))
 
-      state = State.find_by(name: row["detectedstate"])
-      zone_code = formatted_code(state.code) if state.present?
+      process_existing_patient(row) if patient_already_exists
+      process_new_patient(row) unless patient_already_exists
+    end
+  end
 
-      district = District.find_by(name: row["detecteddistrict"])
-      zone_code = formatted_code(district.code) if district.present?
+  def process_existing_patient(row)
+    patient = Patient.find_by(code: Patient.code_for(row["patientnumber"]))
 
-      city = City.find_by(name: row["detectedcity"])
-      zone_code = formatted_code(city.code) if city.present?
+    zone_code = zone_code_for(row)
+    status_change_date = parse_as_date(row["statuschangedate"])
 
-      patient = Patient.new(
-        slug:      slug,
-        code:      code,
+    if (patient.status_changed_on != status_change_date) || patient.zone_code != zone_code
+      patient.assign_attributes(
         status:    row["currentstatus"].parameterize,
-        zone_code: zone_code.nil? ? formatted_code("in") : zone_code,
+        zone_code: zone_code,
         **PATIENT_ATTRS.transform_values { |key| row[key.to_s] }
       )
 
-      source = Source.new(
-        slug:    "covid19-india-#{patient_number}",
-        code:    "c19-in-#{patient_number}",
-        name:    "INDIA COVID-19 TRACKER",
-        details: {
-          source_1: row["source1"],
-          source_2: row["source2"],
-          source_3: row["source3"]
-        }
-      )
-
-      patient.source = source.code if source.save
-
-      next if patient.save
-
-      puts_red "Error: ##{row['patientnumber']} => #{patient.errors.full_messages.to_sentence}"
+      puts_red "Error: ##{row['patientnumber']} => #{patient.errors.full_messages.to_sentence}" unless patient.save
+    else
+      puts_green "Skipping ##{row['patientnumber']}"
     end
+  end
+
+  def process_new_patient(row)
+    patient_number = row["patientnumber"]
+    slug = Patient.slug_for(patient_number)
+    code = Patient.code_for(patient_number)
+
+    patient = Patient.new(
+      slug:      slug,
+      code:      code,
+      status:    row["currentstatus"].parameterize,
+      zone_code: zone_code_for(row),
+      **PATIENT_ATTRS.transform_values { |key| row[key.to_s] }
+    )
+
+    source = new_source_for(row)
+
+    patient.source = source.code if source.save
+    return if patient.save
+
+    source.destroy
+    puts_red "Error: ##{row['patientnumber']} => #{patient.errors.full_messages.to_sentence}"
+  end
+
+  def zone_code_for(row)
+    zone_code = nil
+
+    state = State.find_by(name: row["detectedstate"])
+    zone_code = formatted_code(state.code) if state.present?
+
+    district = District.find_by(name: row["detecteddistrict"])
+    zone_code = formatted_code(district.code) if district.present?
+
+    city = City.find_by(name: row["detectedcity"])
+    zone_code = formatted_code(city.code) if city.present?
+
+    zone_code.nil? ? formatted_code("in") : zone_code
   end
 
   def formatted_code(code)
     code_parts = code.split("/")
-    unknowns = ["unknown"] * (4 - code_parts.count)
+    unknowns = ["unknown"] * (5 - code_parts.count)
     (code_parts + unknowns).join("/")
+  end
+
+  def new_source_for(row)
+    patient_number = row["patientnumber"]
+
+    Source.new(
+      slug:    Source.slug_for(patient_number),
+      code:    Source.code_for(patient_number),
+      name:    "INDIA COVID-19 TRACKER",
+      details: {
+        source_1: row["source1"],
+        source_2: row["source2"],
+        source_3: row["source3"]
+      }
+    )
+  end
+
+  def parse_as_date(val)
+    val.present? ? Date.parse(val) : val
   end
 
   def puts_red(str)
--- app/models/source.rb
+++ app/models/source.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class Source < ApplicationRecord
+  def self.slug_for(uid)
+    "covid19-india-#{uid}"
+  end
+
+  def self.code_for(uid)
+    "c19-in-#{uid}"
+  end
 end
