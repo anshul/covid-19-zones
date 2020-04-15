5900b5983c4c7a34f85cbe1f3dcd8ed19ce3df7c
Jawad Shaikh <shaikhjawad007@gmail.com>
2020-04-15 13:54:35 +0530

    improvement(react): remove container from zone page

--- src/components/ZonePage/ZonePageRoot.tsx
+++ src/components/ZonePage/ZonePageRoot.tsx
@@ -57,72 +57,70 @@ const ZonePageRoot: React.FC<Props> = ({ slug, gotoZone, gotoParentZone }) => {
   console.log(data, data?.fiveDayMovingAverage ?? [])
 
   return (
-    <Container>
-      <Grid container spacing={1}>
-        <Grid item xs={12} md={4}>
-          <Paper>
-            {data?.parentZone && (
-              <div onClick={() => gotoParentZone()} className={clsx(classes.zoneListItem, classes.zoneListParentItem)}>
-                <ArrowBack />
-                <Typography variant='body1' color='textSecondary' className={classes.zoneListParentItemText}>
-                  {data?.parentZone.name}
+    <Grid container spacing={1}>
+      <Grid item xs={12} md={4}>
+        <Paper>
+          {data?.parentZone && (
+            <div onClick={() => gotoParentZone()} className={clsx(classes.zoneListItem, classes.zoneListParentItem)}>
+              <ArrowBack />
+              <Typography variant='body1' color='textSecondary' className={classes.zoneListParentItemText}>
+                {data?.parentZone.name}
+              </Typography>
+            </div>
+          )}
+          {data?.siblingZones.map((zone: any) => (
+            <div key={zone.slug} onClick={() => gotoZone(zone.slug)} className={classes.zoneListItem}>
+              <Typography variant='body1'>
+                {zone.name} ({zone.totalCases})
                 </Typography>
-              </div>
-            )}
-            {data?.siblingZones.map((zone: any) => (
-              <div key={zone.slug} onClick={() => gotoZone(zone.slug)} className={classes.zoneListItem}>
-                <Typography variant='body1'>
-                  {zone.name} ({zone.totalCases})
-                </Typography>
-              </div>
-            ))}
-          </Paper>
-        </Grid>
-        <Grid item xs={12} md={8}>
-          <Paper className={classes.lineChartContainer}>
-            <ResponsiveLine
-              data={[
-                { id: 'Confirmed Cases (Daily)', data: data?.perDayCounts ?? [] },
-                { id: 'Confirmed Cases (3 Day Moving Average)', data: data?.fiveDayMovingAverage ?? [] },
-              ]}
-              margin={{ top: 50, right: 50, bottom: 50, left: 50 }}
-              xScale={{ type: 'point' }}
-              yScale={{ type: 'linear', min: 'auto', max: 'auto', reverse: false }}
-              axisTop={null}
-              axisLeft={null}
-              axisBottom={{
-                orient: 'bottom',
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-              }}
-              axisRight={{
-                orient: 'right',
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-              }}
-              colors={{ scheme: 'nivo' }}
-              pointSize={10}
-              pointBorderColor={{ from: 'serieColor' }}
-              useMesh={true}
-              legends={[
-                {
-                  anchor: 'top-left',
-                  direction: 'column',
-                  itemWidth: 80,
-                  itemHeight: 20,
-                  itemOpacity: 0.75,
-                  symbolSize: 12,
-                  symbolShape: 'circle',
-                  symbolBorderColor: 'rgba(0, 0, 0, .5)',
-                },
-              ]}
-            />
-          </Paper>
-        </Grid>
+            </div>
+          ))}
+        </Paper>
       </Grid>
-    </Container>
+      <Grid item xs={12} md={8}>
+        <Paper className={classes.lineChartContainer}>
+          <ResponsiveLine
+            data={[
+              { id: 'Confirmed Cases (Daily)', data: data?.perDayCounts ?? [] },
+              { id: 'Confirmed Cases (3 Day Moving Average)', data: data?.fiveDayMovingAverage ?? [] },
+            ]}
+            margin={{ top: 50, right: 50, bottom: 50, left: 50 }}
+            xScale={{ type: 'point' }}
+            yScale={{ type: 'linear', min: 'auto', max: 'auto', reverse: false }}
+            axisTop={null}
+            axisLeft={null}
+            axisBottom={{
+              orient: 'bottom',
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+            }}
+            axisRight={{
+              orient: 'right',
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+            }}
+            colors={{ scheme: 'nivo' }}
+            pointSize={10}
+            pointBorderColor={{ from: 'serieColor' }}
+            useMesh={true}
+            legends={[
+              {
+                anchor: 'top-left',
+                direction: 'column',
+                itemWidth: 80,
+                itemHeight: 20,
+                itemOpacity: 0.75,
+                symbolSize: 12,
+                symbolShape: 'circle',
+                symbolBorderColor: 'rgba(0, 0, 0, .5)',
+              },
+            ]}
+          />
+        </Paper>
+      </Grid>
+    </Grid>
   )
 }
 
