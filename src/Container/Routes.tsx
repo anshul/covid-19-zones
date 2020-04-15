e2f0a9cd42193086630bec14be536a57f7a0f122
Danish Shah <shah.danish.1996@gmail.com>
2020-04-16 05:26:05 +0530

    improvement(react): improve zone ui

--- src/Container/Routes.tsx
+++ src/Container/Routes.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { Route, Switch, Redirect } from 'react-router-dom'
-import Home from '../containers/Home'
+// import Home from '../containers/Home'
 import ZonePage from '../components/ZonePage'
 
 const Routes: React.FC = () => {
--- src/components/ZonePage/ZonePageRoot.tsx
+++ src/components/ZonePage/ZonePageRoot.tsx
@@ -5,19 +5,21 @@ import {
   makeStyles,
   createStyles,
   Theme,
-  List,
-  ListItem,
   Grid,
-  ListItemText,
-  ListSubheader,
-  ListItemIcon,
   Typography,
-  Container,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  IconButton,
+  Toolbar,
 } from '@material-ui/core'
 import useSWR from 'swr'
-import { history } from '../../history'
-import { ArrowBack, ArrowForward } from '@material-ui/icons'
+import { ArrowBack } from '@material-ui/icons'
 import clsx from 'clsx'
+import { startCase } from 'lodash'
 
 interface Props {
   slug: string
@@ -27,8 +29,8 @@ interface Props {
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    lineChartContainer: {
-      height: '50vh',
+    lineChart: {
+      height: '30vh',
     },
     zoneList: {
       height: '50vh',
@@ -53,72 +55,157 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const ZonePageRoot: React.FC<Props> = ({ slug, gotoZone, gotoParentZone }) => {
   const classes = useStyles()
-  const { data, error } = useSWR(`/api/zone?slug=${slug}`)
-  console.log(data, data?.seriesAnnouncedSma5 ?? [])
+  const { data } = useSWR(`/api/zone?slug=${slug}`)
+  const colorMap: { [key: string]: { [key: string]: string } } = {
+    active: {
+      color: '#212121',
+      backgroundColor: '#82B1FF',
+      chartColor: '#2196F3',
+    },
+    recovered: {
+      color: '#212121',
+      backgroundColor: '#B9F6CA',
+      chartColor: '#4CAF50',
+    },
+    deceased: {
+      color: '#212121',
+      backgroundColor: '#ff8a80',
+      chartColor: '#f44336',
+    },
+  }
 
   return (
     <Grid container spacing={1}>
+      <Grid item xs={12} md={2} />
       <Grid item xs={12} md={4}>
-        <Paper>
+        <Paper variant='outlined'>
           {data?.parentZone && (
-            <div onClick={() => gotoParentZone()} className={clsx(classes.zoneListItem, classes.zoneListParentItem)}>
-              <ArrowBack />
-              <Typography variant='body1' color='textSecondary' className={classes.zoneListParentItemText}>
+            <Toolbar>
+              <IconButton edge='start' onClick={() => gotoParentZone()} className={clsx(classes.zoneListItem, classes.zoneListParentItem)}>
+                <ArrowBack />
+              </IconButton>
+              <Typography variant='h6' style={{ marginLeft: '16px', fontWeight: 'bold', color: '#0000008a' }}>
                 {data?.parentZone.name}
               </Typography>
-            </div>
+            </Toolbar>
           )}
-          {data?.siblingZones.map((zone: any) => (
-            <div key={zone.slug} onClick={() => gotoZone(zone.slug)} className={classes.zoneListItem}>
-              <Typography variant='body1'>
-                {zone.name} ({zone.totalCases})
-              </Typography>
-            </div>
-          ))}
+          <TableContainer style={{ height: '100vh' }}>
+            <Table stickyHeader aria-label='sticky table'>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Zone</TableCell>
+                  <TableCell>Total</TableCell>
+                  <TableCell>Active</TableCell>
+                  <TableCell>Recovered</TableCell>
+                  <TableCell>Deceased</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data?.siblingZones.map((zone: any) => (
+                  <TableRow selected={slug === zone.slug} hover key={zone.code} onClick={() => gotoZone(zone.slug)}>
+                    <TableCell>{zone.name}</TableCell>
+                    <TableCell>{zone.totalCases}</TableCell>
+                    <TableCell>{zone.totalActive}</TableCell>
+                    <TableCell>{zone.totalRecovered}</TableCell>
+                    <TableCell>{zone.totalDeceased}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Paper>
       </Grid>
-      <Grid item xs={12} md={8}>
-        <Paper className={classes.lineChartContainer}>
-          <ResponsiveLine
-            data={[
-              { id: 'Confirmed Cases (Daily)', data: data?.seriesAnnounced ?? [] },
-              { id: 'Confirmed Cases (5 Day Moving Average)', data: data?.seriesAnnouncedSma5 ?? [] },
-            ]}
-            margin={{ top: 50, right: 50, bottom: 50, left: 50 }}
-            xScale={{ type: 'point' }}
-            yScale={{ type: 'linear', min: 'auto', max: 'auto', reverse: false }}
-            axisTop={null}
-            axisLeft={null}
-            axisBottom={{
-              orient: 'bottom',
-              tickSize: 5,
-              tickPadding: 5,
-              tickRotation: 0,
-            }}
-            axisRight={{
-              orient: 'right',
-              tickSize: 5,
-              tickPadding: 5,
-              tickRotation: 0,
-            }}
-            colors={{ scheme: 'nivo' }}
-            pointSize={10}
-            pointBorderColor={{ from: 'serieColor' }}
-            useMesh={true}
-            legends={[
-              {
-                anchor: 'top-left',
-                direction: 'column',
-                itemWidth: 80,
-                itemHeight: 20,
-                itemOpacity: 0.75,
-                symbolSize: 12,
-                symbolShape: 'circle',
-                symbolBorderColor: 'rgba(0, 0, 0, .5)',
-              },
-            ]}
-          />
-        </Paper>
+      <Grid item spacing={2} xs={12} md={4}>
+        <Grid container spacing={2} item xs={12}>
+          <Grid item xs={12}>
+            {data && <Typography variant='h6'>{data.zone}</Typography>}
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <div style={{ cursor: 'pointer', padding: '8px 16px', color: '#212121', backgroundColor: '#ECEFF1' }}>
+              <Typography variant='subtitle1'>Total</Typography>
+              <Typography variant='h5'>
+                <b>{data?.totalCases || '--'}</b>
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <div style={{ cursor: 'pointer', padding: '8px 16px', color: '#212121', backgroundColor: '#82B1FF' }}>
+              <Typography variant='subtitle1'>Active</Typography>
+              <Typography variant='h5'>
+                <b>{data?.active.totalCount || '--'}</b>
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <div style={{ cursor: 'pointer', padding: '8px 16px', color: '#212121', backgroundColor: '#B9F6CA' }}>
+              <Typography variant='subtitle1'>Recovered</Typography>
+              <Typography variant='h5'>
+                <b>{data?.recovered.totalCount || '--'}</b>
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <div style={{ cursor: 'pointer', padding: '8px 16px', color: '#212121', backgroundColor: '#ff8a80' }}>
+              <Typography variant='subtitle1'>Deceased</Typography>
+              <Typography variant='h5'>
+                <b>{data?.deceased.totalCount || '--'}</b>
+              </Typography>
+            </div>
+          </Grid>
+          {['active', 'recovered', 'deceased'].map((cases) => (
+            <Grid key={cases} item xs={12}>
+              <div className={classes.lineChart}>
+                <ResponsiveLine
+                  colors={[colorMap[cases].chartColor, '#607D8B']}
+                  data={[
+                    { id: `${startCase(cases)} Cases (Daily)`, data: data ? data[cases].perDayCounts : [] },
+                    { id: `${startCase(cases)} Cases (5 Day Moving Average)`, data: data ? data[cases].fiveDayMovingAverage : [] },
+                  ]}
+                  margin={{ top: 60, right: 50, bottom: 60, left: 30 }}
+                  xScale={{
+                    type: 'time',
+                    format: '%Y-%m-%d',
+                    precision: 'day',
+                  }}
+                  xFormat='time:%Y-%m-%d'
+                  yScale={{ type: 'linear', min: 'auto', max: data ? data.yMax : 'auto', reverse: false }}
+                  axisTop={null}
+                  axisLeft={null}
+                  axisBottom={{
+                    orient: 'bottom',
+                    tickSize: 5,
+                    tickPadding: 5,
+                    tickRotation: 90,
+                    format: '%b %d',
+                    tickValues: 'every 2 days',
+                    legend: 'time scale',
+                  }}
+                  axisRight={{
+                    orient: 'right',
+                    tickSize: 5,
+                    tickPadding: 5,
+                    tickRotation: 0,
+                  }}
+                  pointSize={10}
+                  pointBorderColor={{ from: 'serieColor' }}
+                  useMesh={true}
+                  legends={[
+                    {
+                      anchor: 'top-left',
+                      direction: 'column',
+                      itemWidth: 80,
+                      itemHeight: 20,
+                      itemOpacity: 0.75,
+                      symbolSize: 12,
+                      symbolShape: 'circle',
+                      symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                    },
+                  ]}
+                />
+              </div>
+            </Grid>
+          ))}
+        </Grid>
       </Grid>
     </Grid>
   )
