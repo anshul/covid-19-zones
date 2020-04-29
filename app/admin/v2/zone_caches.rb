e27578ee289cef1badd3934e31bde7c98e937792
Jawad Shaikh <shaikhjawad007@gmail.com>
2020-04-29 17:48:53 +0530

    feat(backoffice): add computation download feature

--- app/admin/v2/zone_caches.rb
+++ app/admin/v2/zone_caches.rb
@@ -3,6 +3,7 @@
 ActiveAdmin.register ::V2::ZoneCache, as: "ZoneComputation" do
   menu label: "Zone computations", priority: 15
   includes :zone
+  config.sort_order = "cumulative_infections_desc"
 
   scope :all, default: true do |caches|
     caches.joins(:zone)
@@ -107,6 +108,50 @@ ActiveAdmin.register ::V2::ZoneCache, as: "ZoneComputation" do
           end
         end
       end
+    end
+  end
+
+  TS_CATEGORIES = %w[infections recoveries fatalities].freeze
+  TS_CATEGORIES.each do |category|
+    action_item :"download_daily_#{category}", only: :index do
+      link_to "Daily #{category.humanize} CSV", v2_zone_computations_path({ format: :csv, csv_type: :daily, csv_ts_category: category })
+    end
+  end
+
+  action_item :download_totals, only: :index do
+    link_to "Totals CSV", v2_zone_computations_path({ format: :csv, csv_type: :totals })
+  end
+
+  csv humanize_name: false do
+    csv_type = params[:csv_type]
+    column(:name) { |cache| cache.zone.name }
+    column(:category) { |cache| cache.zone.category }
+    column :code
+    column :population
+    column :area_sq_km
+    column :as_of
+
+    if csv_type == "totals"
+      column :current_actives
+      column :cumulative_infections
+      column :cumulative_recoveries
+      column :cumulative_fatalities
+      column :cumulative_tests
+    end
+
+    if csv_type == "daily"
+      category = TS_CATEGORIES.include?(params[:csv_ts_category]) ? params[:csv_ts_category] : "infections"
+      (Date.parse("30-01-2020")..Date.tomorrow).each do |date|
+        column(date.to_s) { |cache| cache.send(:"ts_#{category}")[date.to_s].to_i }
+      end
+    end
+  end
+
+  controller do
+    def csv_filename
+      csv_type = params[:csv_type]
+      csv_ts_category = params[:csv_ts_category]
+      "zone-computations-#{csv_type}#{csv_ts_category.present? ? "-#{csv_ts_category}" : nil}-#{Time.zone.today}.csv"
     end
   end
 
