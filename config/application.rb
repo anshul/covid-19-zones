dec4e52d68652fa144a07a1561f152b9128f8796
Jawad Shaikh <shaikhjawad007@gmail.com>
2020-05-21 13:23:14 +0530

    perf(v2): add memcached caching and deflater middleware

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module Covid19Zones
       end
     end
 
+    config.middleware.use Rack::Deflater
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
