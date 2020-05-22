b86097fc7fb687b489108bb4eaa0175cd8517a7f
Anshul Khandelwal <anshul@anshul.io>
2020-05-22 13:55:18 +0530

    feat(v2): provide category in gql

--- app/models/v2/zone.rb
+++ app/models/v2/zone.rb
@@ -23,6 +23,10 @@ module V2
     delegate :per_million_actives, :per_million_fatalities, :per_million_infections, :per_million_recoveries, :per_million_tests, to: :cache
     delegate :chart, :unit_codes, to: :cache
 
+    def p_category
+      category.pluralize
+    end
+
     def related
       return @related_zones if @related_zones
 
